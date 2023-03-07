5eb3a9e9cfb9713fd5aab726dbaabb845b7c864b
Tasmia Rahman <tasmiata@gmail.com>
2023-03-08 00:29:30 +0200

    fix: update query and make it more dynamic

--- server/controllers/stationTypes.js
+++ server/controllers/stationTypes.js
@@ -1,27 +1,6 @@
 // Import database connection pool
 import pool from "../config/database.js";
 
-// Helper Function to update the user data
-const updateAccount = async (id, data) => {
-  const keys = Object.keys(data);
-
-  const info = keys.map((key, i) => `${key} = $${i + 1}`).join(", ");
-
-  const values = keys.map((key) => data[key]);
-
-  console.log("⚠️⚠️⚠️⚠️⚠️⚠️⚠️ values", values);
-  console.log("⚠️⚠️⚠️⚠️⚠️⚠️⚠️ values", keys.length);
-
-  const res = await pool.query(
-    `UPDATE StationType
-		SET ${info}
-		WHERE id = $${keys?.length + 1}`,
-    [...values, id]
-  );
-
-  return res;
-};
-
 // @route   POST /stationTypes
 // @desc    Get all Station Types
 const getStationTypes = async (req, res) => {
@@ -104,10 +83,6 @@ const updateStationType = async (req, res) => {
       params: { id },
     } = req;
 
-    console.log("✅✅✅✅✅", req.body);
-    console.log("✅✅✅✅✅ id", id);
-
-    // Check if name or maxpower is present in the request body
     if (!name && !maxpower) {
       return res.status(400).json({
         success: false,
@@ -115,58 +90,45 @@ const updateStationType = async (req, res) => {
       });
     }
 
-    const result = await updateAccount(id, {
-      name,
-    });
+    const setClause = [];
+    const values = [];
 
-    // const setClause = [];
-    // const values = [];
+    if (name && maxpower) {
+      setClause.push("name=$1, maxpower=$2");
+      values.push(name, maxpower);
+    } else if (name) {
+      setClause.push("name=$1");
+      values.push(name);
+    } else if (maxpower) {
+      setClause.push("maxpower=$1");
+      values.push(maxpower);
+    }
 
-    // if (name) {
-    //   setClause.push("name=$1");
-    //   values.push(name);
-    // } else if (maxpower) {
-    //   setClause.push("maxpower=$1");
-    //   values.push(maxpower);
-    // } else if (name && maxpower) {
-    //   setClause.push("name=$1, maxpower=$2");
-    //   values.push(name, maxpower);
-    // }
+    // Combine setClause into comma separated string
+    const setClauseStr = setClause.join(",");
 
-    // // Combine setClause into comma separated string
-    // const setClauseStr = setClause.join(",");
+    const query = {
+      text: `UPDATE StationType SET ${setClauseStr} WHERE id=$${
+        values.length + 1
+      } RETURNING *`,
+      values: [...values, id],
+    };
 
-    // const query = {
-    //   text: `UPDATE StationType SET ${setClauseStr} WHERE id=$${
-    //     values.length + 1
-    //   } RETURNING *`,
-    //   values: [...values, id],
-    // };
-
-    // const query = {
-    //   text: "UPDATE StationType SET name=$1, maxpower=$2 WHERE id=$3 RETURNING *",
-    //   values: [name, maxpower, id],
-    // };
-    // const query = {
-    //   text: "UPDATE StationType SET ${setClauseStr} WHERE id=$3 RETURNING *",
-    //   values: [name, maxpower, id],
-    // };
-
-    // const result = await pool.query(query);
+    const result = await pool.query(query);
 
     if (result.rowCount > 0) {
-      console.log("⛔️⛔️⛔️⛔️⛔️ ~ result", result.rows);
-      res.status(200).json({ success: true, message: "Update successfully" });
+      res.status(200).json({ success: true, message: "Update successful" });
     } else {
-      res
-        .status(404)
-        .json({ success: false, message: `Station with ID ${id} not found.` });
+      res.status(404).json({
+        success: false,
+        message: `StationType with ID ${id} not found`,
+      });
     }
   } catch (err) {
     console.error(err);
     res.status(500).json({
       success: false,
-      message: `Sever error while trying to update Station with ID ${id} in database: ${err.message}`,
+      message: `Server error while trying to update StationType with ID ${id}: ${err.message}`,
     });
   }
 };
