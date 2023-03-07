1080837a876945ed40d1243ef465c1f0694abfc3
Tasmia Rahman <tasmiata@gmail.com>
2023-03-08 00:50:26 +0200

    feat: add some comments

--- server/config/database.js
+++ server/config/database.js
@@ -7,8 +7,8 @@ const pool = new Pool({
   user: process.env.DB_USERNAME || "postgres",
   password: process.env.DB_PASSWORD || 123456,
   database: process.env.DB_NAME || "charging_station_system",
-  host: process.env.DB_HOST || "db",
-  port: process.env.DB_PORT || 5432,
+  host: process.env.DB_HOST || "db", // localhost for testing
+  port: process.env.DB_PORT || 5432, // 5433 for testing
 });
 
 pool.on("connection", (connection) => {
